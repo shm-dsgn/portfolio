5aee8fd9620fe72bc5a1969bc0c7611ca1eda218
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-05-18 09:42:10 +0530

    Added content in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./Home.css";
+import { MouseSimple } from "@phosphor-icons/react";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="homepage">
+      <div className="home">
+      <div className="hero-text">
+        I <span className="magic-text">code</span> and
+        <span className="magic-text"> design</span> cool stuff for the
+        <span className="magic-text"> web</span>.
+      </div>
+      <div className="hero-sub-text">
+        <p>
+          Hi, I'm <span className="name">Soham Dutta</span>, a
+          <span className="role"> Frontend Developer and User Experience designer </span>
+          based in <span className="location">Kolkata, IN</span>.
+        </p>
+      </div>
+      <div className="scroll-text">
+        <MouseSimple size={24} />
+        <p>Scroll to know more about me.</p>
+      </div>
+    </div>
+    <div className="home-details">
+      Home Details
+    </div>
+    </div>
+  );
 }
 
 export default Home;
