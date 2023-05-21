80a032e02c53fc4d7af5d51d19583dcbe4fbdf5b
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-05-21 21:57:04 +0530

    Added resume redirect

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import ScrollToTop from "./components/ScrollToTop";
+import Resume from "./components/Resume";
 
 function handleMouseMove(e) {
   var cursor = document.querySelector(".cursor");
@@ -31,6 +32,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
       </ScrollToTop>
     </div>
---
+++ src/components/Resume.js
@@ -0,0 +1,5 @@
+function Resume() {
+  window.location.replace("https://drive.google.com/file/d/1-1-9ht1R1086T9KF3m0Ooy4swHL-NFPv/view?usp=share_link")
+}
+
+export default Resume
